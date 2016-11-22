0ebc4b6a6dee3791d9cbdd2405f11dd7e1826f53
yaya <yu_game@qq.com>
2016-11-22 17:19:16 +0800

    静态资源目录配置化

--- app.js
+++ app.js
@@ -22,10 +22,16 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+if(G_config.www) {
+    app.use(express.static(path.join(__dirname, G_config.www)));
+}
+else{
+    app.use(express.static(path.join(__dirname, 'public')));
+}
 
 app.use('/', routes);
 
